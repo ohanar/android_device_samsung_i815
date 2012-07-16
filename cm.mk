e34679b5114f300ad17b3ceea2c35492863b9814
R. Andrew Ohana <andrew.ohana@gmail.com>
2012-07-16 02:22:05 -0700

    clean up cm.mk and i815.mk
    Change-Id: I152018494357c47a4916c751d0f484371cea4b8b

--- cm.mk
+++ cm.mk
@@ -14,20 +14,16 @@
 # limitations under the License.
 #
 
-# Release name
-PRODUCT_RELEASE_NAME := SCH-I815
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
-
 # Inherit device configuration
 $(call inherit-product, $(LOCAL_PATH)/i815.mk)
 
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := i815
-PRODUCT_NAME := cm_i815
+# Device identifier. This must come after all inclusions
 PRODUCT_BRAND := Samsung
+PRODUCT_DEVICE := i815
+PRODUCT_MANUFACTURER := Samsung
 PRODUCT_MODEL := SCH-I815
+PRODUCT_NAME := cm_i815
+PRODUCT_RELEASE_NAME := SCH-I815
 
-#Set build fingerprint / ID / Prduct Name ect.
+# Set product name and build fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SCH-I815 BUILD_FINGERPRINT="samsung/SCH-I815/SCH-I815:3.2/HTJ85B/I815FA02:user/release-keys" PRIVATE_BUILD_DESC="SCH-I815-user 3.2 HTJ85B I815FA02 release-keys"
--- i815.mk
+++ i815.mk
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+
 $(call inherit-product, vendor/cm/config/cdma.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -25,10 +27,3 @@ $(call inherit-product-if-exists, vendor/samsung/smdk4210-tab/smdk4210-tab_base.
 $(call inherit-product, $(LOCAL_PATH)/i815_base.mk)
 
 $(call inherit-product-if-exists, vendor/samsung/i815/i815_base.mk)
-
-# Discard inherited values and use our own instead.
-PRODUCT_NAME := i815
-PRODUCT_DEVICE := i815
-PRODUCT_BRAND := samsung
-PRODUCT_MANUFACTURER := samsung
-PRODUCT_MODEL := SCH-I815
